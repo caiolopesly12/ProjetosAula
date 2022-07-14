27e0b9d75f2ca02fde6cacf5c7f51d253a68abbc
caiolopesly12 <106359104+caiolopesly12@users.noreply.github.com>
2022-07-14 08:22:32 -0300

    Add files via upload

---
+++ Caderno do Caio-131 de aulas/Aula 6/index.php
@@ -0,0 +1,13 @@
+<html>
+
+<?php
+
+//variaveis
+$titulo = <h1>"Primeira aula de PhP"</h1> ;
+
+echo "$titulo"  
+echo <h2> "Olá mundão"</h2>;
+echo "echo é melhor que print" <br>;
+?>
+<html>
+    
