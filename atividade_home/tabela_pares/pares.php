265a00485e6b602a820b681a8dbd987b155db34e
caiolopesly12 <106359104+caiolopesly12@users.noreply.github.com>
2022-07-27 23:01:16 -0300

    Add files via upload

--- atividade_home/tabela_pares/pares.php
+++ atividade_home/tabela_pares/pares.php
@@ -1,24 +1,27 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-echo 
 
 
+    if(isset($_POST["num"])) {
+        echo $_POST["num"];
+        echo "<br>";  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $num = $_POST["num"];
+        
+    if($num % 2 == 0) {
+        echo "O numero informado é par";
+    }
+    else {
+        echo "O numero infoormado é impar";
+    }
+}
 
 
 
@@ -26,3 +29,39 @@ echo
 
 
 ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
