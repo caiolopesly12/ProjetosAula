d71b41a303345d853f480ad07655fb4ffe0913ce
caiolopesly12 <106359104+caiolopesly12@users.noreply.github.com>
2022-09-21 10:53:06 -0300

    Add files via upload

--- caio141_trabalho2/principal.php
+++ caio141_trabalho2/principal.php
@@ -8,129 +8,53 @@
 </head>
 <body>
     <?php
-        $op = $_GET['op'];
+       $op = $_GET['op'];
        switch ($op) {
-        case 1 :
-            echo "
-            <fieldset>
-            <legend><h1>Iniciar o Cliente</h1></legend>
-            <form action='calculo.php'  method ='GET'>
-                Código: <input type ='number' name ='codigo'>
-                <br> 
-                Nome: <input type ='text' name ='nome'>
-                <br>
-                Nascimento: <input type ='date' name ='nasciemnto'>
-                <br>
-                Endereço: <input type ='text' name ='enedereco'>
-                <br>
-                Cpf: <input type ='number' name ='cpf'>
-                <br>  
-                <input type = hidden name =op value= $op>     
-                <button type='submit'>Enviar</button>
-                <button type='reset'>Limpar</button>
-            </form>
-            </fieldset> 
-            </body>
-            ";
-           
+            case 1 :
+                if (isset($_GET['enviado'])) {
+                    $cod = $_GET['codigo'];
+                    echo"$cod <br>
+                    Cadastro enviado com sucesso!
+                    ";
+                    $nom = $_GET['nascimento'];
+                    $end = $_GET['endereco'];
+                    $cpf = $_GET['cpf'];
+                } else {
+                    
+                    $sql = "insert into clienete values ('codigo' , 'nome' , 'nascimento' , 'endereco' ,'cpf'" 
+                    echo $sql;
+                    $con = msqli_connect("127.0.0.1")
+                    $stat = msqli_query($con, $sql)
+                    if($stat = true){
+                echo "
+                    <fieldset>
+                    <legend><h1>Iniciar o Cliente</h1></legend>
+                    <form action='principal.php'  method ='GET'>
+                        Código: <input type ='number' name ='codigo'>
+                        <br> 
+                        Nome: <input type ='text' name ='nome'>
+                        <br>
+                        Nascimento: <input type ='date' name ='nascimento'>
+                        <br>
+                        Endereço: <input type ='text' name ='endereco'>
+                        <br>
+                        Cpf: <input type ='number' name ='cpf'>
+                        <br>   
+                        <input type='hidden' name='enviado' value='s'>
+                        <input type='hidden' name='op' value='1'>
+                        <button type='submit'>Enviar</button>
+                        <button type='reset'>Limpar</button>
+                    </form>
+                    </fieldset> 
+                    </body>
+                    ";
+                }
             break;
-        case 2 :
-            echo "
-            <fieldset>
-            <legend><h1>Pesquisar o Cliente</h1></legend>
-            <form action='calculo.php'  method ='GET'>
-                Código: <input type ='number' name ='codigo'>
-                <br> 
-                Nome: <input type ='text' name ='nome'>
-                <br>
-                Nascimento: <input type ='date' name ='nasciemnto'>
-                <br>
-                Endereço: <input type ='text' name ='enedereco'>
-                <br>
-                Cpf: <input type ='number' name ='cpf'>
-                <br>  
-                <input type = hidden name =op value= $op>     
-                <button type='submit'>Enviar</button>
-                <button type='reset'>Limpar</button>
-            </form>
-            </fieldset> 
-            </body>
-            ";
-            
-            break;
-        case 3:
-            echo "
-            <fieldset>
-            <legend><h1>Listar o Cliente</h1></legend>
-            <form action='calculo.php'  method ='GET'>
-                Código: <input type ='number' name ='codigo'>
-                <br> 
-                Nome: <input type ='text' name ='nome'>
-                <br>
-                Nascimento: <input type ='date' name ='nasciemnto'>
-                <br>
-                Endereço: <input type ='text' name ='enedereco'>
-                <br>
-                Cpf: <input type ='number' name ='cpf'>
-                <br>  
-                <input type = hidden name =op value= $op>     
-                <button type='submit'>Enviar</button>
-                <button type='reset'>Limpar</button>
-            </form>
-            </fieldset> 
-            </body>
-            
-            ";
-            break;
+            }
+            case 2 :
+              
 
-        case 4:
-            echo "
-            <fieldset>
-            <legend><h1>Excluir o Cliente</h1></legend>
-            <form action='calculo.php'  method ='GET'>
-                Código: <input type ='number' name ='codigo'>
-                <br> 
-                Nome: <input type ='text' name ='nome'>
-                <br>
-                Nascimento: <input type ='date' name ='nasciemnto'>
-                <br>
-                Endereço: <input type ='text' name ='enedereco'>
-                <br>
-                Cpf: <input type ='number' name ='cpf'>
-                <br>  
-                <input type = hidden name =op value= $op>     
-                <button type='submit'>Enviar</button>
-                <button type='reset'>Limpar</button>
-            </form>
-            </fieldset> 
-            </body>
-            ";
             break;
-
-        case 5:
-            echo "
-            <fieldset>
-            <legend><h1>Alterar o Cliente</h1></legend>
-            <form action='calculo.php'  method ='GET'>
-                Código: <input type ='number' name ='codigo'>
-                <br> 
-                Nome: <input type ='text' name ='nome'>
-                <br>
-                Nascimento: <input type ='date' name ='nasciemnto'>
-                <br>
-                Endereço: <input type ='text' name ='enedereco'>
-                <br>
-                Cpf: <input type ='number' name ='cpf'>
-                <br>  
-                <input type = hidden name =op value= $op>     
-                <button type='submit'>Enviar</button>
-                <button type='reset'>Limpar</button>
-            </form>
-            </fieldset> 
-            </body>
-            ";
-            break;
-   }
 
     ?>
 </html>
