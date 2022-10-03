d964f6aa466f93fb31dce969c5607209d8d0a3a8
caiolopesly12 <106359104+caiolopesly12@users.noreply.github.com>
2022-10-03 16:16:14 -0300

    Add files via upload

--- caio141_trabalho2/principal.php
+++ caio141_trabalho2/principal.php
@@ -13,19 +13,18 @@
             case 1 :
                 if (isset($_GET['enviado'])) {
                     $cod = $_GET['codigo'];
-                    echo"$cod <br>
-                    Cadastro enviado com sucesso!
-                    ";
-                    $nom = $_GET['nascimento'];
+                    $nas = $_GET['nascimento'];
+                    $nom = $_GET['nome'];
                     $end = $_GET['endereco'];
                     $cpf = $_GET['cpf'];
+                    $sql = "insert into cliente values ('$cod' , '$nom' , '$nas' , '$end' ,'$cpf')";
+                    echo $sql;
+                    $con = mysqli_connect("localhost", "root", "", "sus");
+                    $stat = mysqli_query($con, $sql);
                 } else {
                     
-                    $sql = "insert into clienete values ('codigo' , 'nome' , 'nascimento' , 'endereco' ,'cpf'" 
-                    echo $sql;
-                    $con = msqli_connect("127.0.0.1")
-                    $stat = msqli_query($con, $sql)
-                    if($stat = true){
+                    
+                    
                 echo "
                     <fieldset>
                     <legend><h1>Iniciar o Cliente</h1></legend>
@@ -41,20 +40,93 @@
                         Cpf: <input type ='number' name ='cpf'>
                         <br>   
                         <input type='hidden' name='enviado' value='s'>
-                        <input type='hidden' name='op' value='1'>
+                        <input type='hidden' name='op' value='$op'>
                         <button type='submit'>Enviar</button>
-                        <button type='reset'>Limpar</button>
+                        <button type='reset'>Limpar</button> <br>
+                        <button><a href='inicial.html'>Voltar</a></button>
                     </form>
                     </fieldset> 
                     </body>
                     ";
                 }
             break;
-            }
             case 2 :
-              
+                if ($_GET['entrada'] == 1) {
+                    echo "
+                    <form action='principal.php'  method ='GET'>
+                        Código:<input type='number' name='codigo'> 
+                        <input type='hidden' name='op' value='$op'>
+                        <br>
+                        <input type='hidden' name='entrada' value='2'>
+                        <button type='submit'>Enviar</button>
+                        <button type='reset'>Limpar</button>
+                        </form>
+                        
+                    ";
+                } else {
+                    $cod = $_GET['codigo'];
+                    $sql = "select * from cliente where codigo = $cod";
+                    $con = mysqli_connect("localhost", "root", "", "sus");
+                    $dados = mysqli_query($con, $sql);
+                    $quant = mysqli_num_rows($dados);
+                    if($quant > 0) {
+                        $linha = mysqli_fetch_array($dados);
+                        $cod = $linha['codigo'];
+                        $nom = $linha['nome'];
+                        $nas = $linha['nascimento'];
+                        $end = $linha['endereco'];
+                        $cpf = $linha['cpf'];
 
+                        echo "
+                            <p>Código: $cod</p>
+                            <p>Nome: $nom</p>
+                            <p>Nascimento: $nas</p>
+                            <p>Endereço: $end</p>
+                            <p>CPF: $cpf</p>
+                            <button><a href='inicial.html'>Voltar</a></button>
+                        ";
+                    } else {
+                        echo "Nada encontrado";
+                    }
+            }
             break;
+        case 3 :
+                    $sql = "select * from cliente";
+                    $con = mysqli_connect("localhost", "root", "", "sus");
+                    $dados = mysqli_query($con, $sql);
+                    $quant = mysqli_num_rows ($dados);
+                    if($quant > 0) {
+                    for ($i=0 ; $i<$quant ; $i++){
+                    $linha = mysqli_fetch_array($dados);
+                    $cod = $linha['codigo'];
+                    $nom = $linha['nome'];
+                    $nas = $linha['nascimento'];
+                    $end = $linha['endereco'];
+                    $cpf = $linha['cpf'];
 
+                
+                echo "
+                <fieldset>
+                    <legend><h1>Listar o Cliente</h1></legend>
+                    <form>
+                    Código:$cod 
+                    <br>
+                    Nome:$nom 
+                    <br>
+                    Nascimento:$nas 
+                    <br>
+                    Endereço:$end 
+                    <br>
+                    Cpf:$cpf
+                    <br>
+                    <button><a href='inicial.html'>Voltar</a></button>
+                    </form>
+                </fieldset> 
+                
+                ";
+                }
+
+                }
+            }
     ?>
 </html>
