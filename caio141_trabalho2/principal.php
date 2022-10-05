a12552e4e177db71ff0823d22a976798614501db
caiolopesly12 <106359104+caiolopesly12@users.noreply.github.com>
2022-10-05 10:53:42 -0300

    Add files via upload

--- caio141_trabalho2/principal.php
+++ caio141_trabalho2/principal.php
@@ -59,7 +59,8 @@
                         <br>
                         <input type='hidden' name='entrada' value='2'>
                         <button type='submit'>Enviar</button>
-                        <button type='reset'>Limpar</button>
+                        <button type='reset'>Limpar</button> <br>
+                        <button><a href='inicial.html'>Voltar</a></button>
                         </form>
                         
                     ";
@@ -126,7 +127,168 @@
                 ";
                 }
 
+            }   
+            break;
+        case 4 :
+            if($_GET['entrada'] == 1) {
+                        
+                echo "
+                    <form>
+                    <label for='codigo'>Código:</label>
+                    <input type='number' name='codigo'>
+                    <input type='hidden' name='op' value='$op'> 
+                    <input type='hidden' name='entrada' value='2'> 
+                    <br>
+                    <button type='submit'>Enviar</button>
+                    <button type='reset'>Limpar</button>
+                    </form>
+                    <button><a href='inicial.html'>Voltar</a></button>
+                ";
+                } elseif($_GET['entrada'] == 2) {
+                    $cod = $_GET['codigo'];
+                    $sql = "select * from cliente where codigo = $cod";
+                    $con = mysqli_connect("localhost", "root", "", "sus");
+                    $dados = mysqli_query($con, $sql);
+                    $quantidade = mysqli_num_rows($dados);
+
+                    if($quantidade > 0) {
+                        $linha = mysqli_fetch_array($dados);  
+                        $cod = $linha['codigo'];
+                        $nom = $linha['nome'];
+                        $nas = $linha['nascimento'];
+                        $end = $linha['endereco'];
+                        $cpf = $linha['cpf'];
+        
+                        echo "
+                            <form>
+                            <fieldset>
+                            <legend><h1>Cliente encontrado!</h1></legend>
+                            Código: $cod
+                            <br>
+                            Nome: $nom
+                            <br>
+                            Nascimento: $nas
+                            <br>
+                            Endereço: $end
+                            <br>
+                            CPF: $cpf  
+                            <br>
+                            <br>
+                            <p>Tem certeza que deseja excluir este cliente?</p>
+    
+                            <input type='radio' name='escolha' value='1'>Sim
+                            <input type='radio' name='escolha' value='2'>Não
+    
+                            <br>
+
+                            <input type=hidden name=op value=$op> 
+                            <input type=hidden name=entrada value=3>
+                            <input type=hidden name=codigo value=$cod>
+
+                            <button type='submit'>Enviar</button>
+                            <button type='reset'>Limpar</button> <br>
+                            <button><a href='inicial.html'>Voltar</a></button>
+                            </fieldset>
+                            </form>    
+                            
+                            
+                         
+                        ";
+                }  else {
+                    echo "
+                        <p>Código de cliente não encontrado</p>
+                    ";
                 }
             }
+            if ($_GET['entrada'] == 3) {
+                
+                    $cod = $_GET['codigo'];
+                    $escolha = $_GET ['escolha'];
+        
+                    if ($escolha == 2) {
+                    header("Location: principal.php?op=4&entrada=1"); 
+                    exit();
+                }
+                
+                    $sql = "delete from cliente where codigo = $cod";
+                    $con = mysqli_connect("localhost", "root", "", "sus");
+                    $dados = mysqli_query($con, $sql); 
+                    $status = mysqli_query($con, $sql);
+        
+                    if($status = true) {
+                    echo" REGISTRO EXCLUIDO COM SUCESSO!! <br>
+                    <button><a href='inicial.html'>Voltar</a></button> ";
+                }   else {
+                    echo" HOUVE UM ERRO AO TENTAR EXCLUIR OS DADOS! (Verifique o sql) <br>
+                    <button><a href='inicial.html'>Voltar</a></button> ";
+                }
+            
+            }
+            break;     
+            case 5 :
+                if($_GET['entrada'] == 1) {
+                            
+                    echo "
+                        <form>
+                        <label for='codigo'>Código:</label>
+                        <input type='number' name='codigo'>
+                        <input type='hidden' name='op' value='$op'> 
+                        <input type='hidden' name='entrada' value='2'> 
+                        <br>
+                        <button type='submit'>Enviar</button>
+                        <button type='reset'>Limpar</button>
+                        </form>
+                        <button><a href='inicial.html'>Voltar</a></button>
+                    ";
+                    } elseif($_GET['entrada'] == 2) {
+                        $cod = $_GET['codigo'];
+                        $sql = "select * from cliente where codigo = $cod";
+                        $con = mysqli_connect("localhost", "root", "", "sus");
+                        $dados = mysqli_query($con, $sql);
+                        $quantidade = mysqli_num_rows($dados);
+    
+                        if($quantidade > 0) {
+                            $linha = mysqli_fetch_array($dados);  
+                            $cod = $linha['codigo'];
+                            $nom = $linha['nome'];
+                            $nas = $linha['nascimento'];
+                            $end = $linha['endereco'];
+                            $cpf = $linha['cpf'];
+                            echo "
+                            <form>
+                            <fieldset>
+                                <legend><h1>Alterar cliente</h1></legend>
+                                CODIGO : $cod <BR>
+                                NOME : <input type='text' name='nome' value='$nom'> <BR> 
+                                NASCIMENTO : <input type='date' name='nascimento' value='$nas'> <BR> 
+                                ENDEREÇO : <input type=text name=endereco value='$end'> <BR>
+                                CPF : <input type=text name=cpf value='$cpf'> <BR>
+        
+                                <br>
+        
+                                <button type='submit'>Enviar</button>
+                                
+                                <input type=hidden name=op value=$op> 
+                                <input type=hidden name=entrada value=3>
+                                <input type=hidden name=codigo value=$cod>
+                                </form>
+                                </fieldset>
+                            ";
+                        }
+                    } elseif ($_GET['entrada'] == 3) {
+                        $cod = $_GET['codigo'];
+                        $nom = $_GET['nome'];
+                        $nas = $_GET['nascimento'];
+                        $end = $_GET['endereco'];
+                        $cpf = $_GET['cpf'];
+
+                        $sql = "update cliente set nome='$nom', nascimento='$nas', endereco='$end', cpf='cpf' where codigo='$cod'";
+                        $con = mysqli_connect("localhost", "root", "", "sus");
+                        $stat = mysqli_query($con, $sql);
+                        echo "Dados alterados";
+                    }
+            
+
+    } 
     ?>
 </html>
